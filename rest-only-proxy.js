fb46d0f18d7994096f35a7cf88411f96a0606e05
tyr7z <tyr7z@users.noreply.github.com>
2025-07-08 23:37:39 +0200

    rest-only-proxy fix

--- rest-only-proxy.js
+++ rest-only-proxy.js
@@ -1,6 +1,6 @@
-const http = require("http");
-const https = require("https");
-const { URL } = require("url");
+import http from "http";
+import https from "https";
+import { URL } from "url";
 
 const PORT = 3001;
 
